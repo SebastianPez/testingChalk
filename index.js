e8cebc70091c529655baa5730dcafd24ee7159e1
SebastianPez <Sebastian.Pezarro@gmail.com>
2019-03-08 19:42:02 +0000

    Added background color

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var chalk = require("chalk");
 
 var message = "Hello " + chalk.yellow("World");
 console.log(message);
+
+var message2 = chalk.blue.bgYellow.bold('Hello world!');
+console.log(message2);
